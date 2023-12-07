5f6927dc9c05a5da6fcb6642d391d0b3005c6ddb
AksharaMukundSonale <91076766+AksharaMukundSonale@users.noreply.github.com>
2023-12-07 20:02:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,4 +18,43 @@ function take_snapshot()
 
 console.log('ml5.version', ml5.version);
 
-classsifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/F4571RVn4/model.json' , modelLoaded)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/iYr1ZT7VO/model.json' , modelLoaded)
+function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+  function gotResult(error, results)
+  {
+    if (error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        speak()
+        if (prediction_1 == "Amazing")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+       else if (prediction_1 == "Best")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+      else if (prediction_1 == "Victory")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+    }
+  }
+
+  function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "The prediction is " + prediction_1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+  }
